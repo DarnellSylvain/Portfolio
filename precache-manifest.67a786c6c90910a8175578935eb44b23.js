d8f08db77e677c399dfe9bcca21dbd28c6308cc3
DarnellS <Darnellsylvain@gmail.com>
2020-10-01 15:20:20 +0100

    Updates

---
+++ precache-manifest.67a786c6c90910a8175578935eb44b23.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fdc58084a183243dae0bc8469cc782a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "51c416c2b2b050eb80d6",
+    "url": "/Portfolio/static/css/2.dd1866a9.chunk.css"
+  },
+  {
+    "revision": "cdf999e66d07c354a176",
+    "url": "/Portfolio/static/css/main.8c8cc57b.chunk.css"
+  },
+  {
+    "revision": "51c416c2b2b050eb80d6",
+    "url": "/Portfolio/static/js/2.8eef9ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.8eef9ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf999e66d07c354a176",
+    "url": "/Portfolio/static/js/main.590a02e6.chunk.js"
+  },
+  {
+    "revision": "9bb5a14c4411ad556432",
+    "url": "/Portfolio/static/js/runtime-main.d985277c.js"
+  },
+  {
+    "revision": "f47c62af163efa5a37ec2a4a13662955",
+    "url": "/Portfolio/static/media/CounterAppV4.f47c62af.jpg"
+  },
+  {
+    "revision": "fd22413cace16edde9a10e7b6ca8cb31",
+    "url": "/Portfolio/static/media/CounterAppV4.fd22413c.webp"
+  },
+  {
+    "revision": "5489dadf7c36869488e1518cb6c4c96e",
+    "url": "/Portfolio/static/media/DarnellPDF.5489dadf.pdf"
+  },
+  {
+    "revision": "36ec9c9c6a05459d956e2f45e28d18d4",
+    "url": "/Portfolio/static/media/ExchangeAppv4.36ec9c9c.jpg"
+  },
+  {
+    "revision": "815794e67a944afe4a8577b654028857",
+    "url": "/Portfolio/static/media/ExchangeAppv4.815794e6.webp"
+  },
+  {
+    "revision": "15de750c11eeda87e81e5625d75c22ce",
+    "url": "/Portfolio/static/media/RandomQuoteV3.15de750c.webp"
+  },
+  {
+    "revision": "83bc499635483fdda3ac1ffdff6bea62",
+    "url": "/Portfolio/static/media/RandomQuoteV3.83bc4996.jpg"
+  },
+  {
+    "revision": "7c7c64a6873d22d73ac25890cf81b82c",
+    "url": "/Portfolio/static/media/WeatherAppV4.7c7c64a6.webp"
+  },
+  {
+    "revision": "c37e9baeb4439588ebd9be6a5dda594d",
+    "url": "/Portfolio/static/media/WeatherAppV4.c37e9bae.jpg"
+  },
+  {
+    "revision": "058e65d2adca81579638fb4fdfd93548",
+    "url": "/Portfolio/static/media/maynoothIMG3.058e65d2.webp"
+  },
+  {
+    "revision": "81903d83459dc3731c7cfdf21797831e",
+    "url": "/Portfolio/static/media/maynoothIMG3.81903d83.jpg"
+  },
+  {
+    "revision": "00cb4eab6bd495cc74cad35bbbfeea3a",
+    "url": "/Portfolio/static/media/maynoothIMGV1.00cb4eab.jpg"
+  },
+  {
+    "revision": "b9275d86350b2c3e66e38d6d235f1288",
+    "url": "/Portfolio/static/media/maynoothIMGV1.b9275d86.webp"
+  },
+  {
+    "revision": "26b129f088be554228123b6e7a8a065f",
+    "url": "/Portfolio/static/media/maynoothIMGV4.26b129f0.webp"
+  },
+  {
+    "revision": "eb43aaa5673cd7721b7bb403f525daa9",
+    "url": "/Portfolio/static/media/maynoothIMGV4.eb43aaa5.jpg"
+  },
+  {
+    "revision": "281cb61ecfdf499c406886cb01c6b1f4",
+    "url": "/Portfolio/static/media/maynoothv2.281cb61e.webp"
+  },
+  {
+    "revision": "45108187b3697bc562fefe2a084cce13",
+    "url": "/Portfolio/static/media/maynoothv2.45108187.jpg"
+  },
+  {
+    "revision": "16cd5e5004e1aa815a947a34fb20abe4",
+    "url": "/Portfolio/static/media/selfie.16cd5e50.webp"
+  },
+  {
+    "revision": "66800eb75c7a1fed123e2ee65674bc0e",
+    "url": "/Portfolio/static/media/selfie.66800eb7.jpg"
+  },
+  {
+    "revision": "610a2c0bd9c33b32ccdf64e92fc1920b",
+    "url": "/Portfolio/static/media/showcaseImage@2x.610a2c0b.webp"
+  },
+  {
+    "revision": "66ade72de18090ab6da1093a4122ded7",
+    "url": "/Portfolio/static/media/showcaseImage@2x.66ade72d.png"
+  },
+  {
+    "revision": "a6e75383595f44322489dc033833819e",
+    "url": "/Portfolio/static/media/supercampIndex.a6e75383.jpg"
+  },
+  {
+    "revision": "b8f86d47b38fbea35d18c048fec61ff3",
+    "url": "/Portfolio/static/media/supercampIndex.b8f86d47.webp"
+  },
+  {
+    "revision": "21d6b90354a041d7f8dd4b1d75ac52fe",
+    "url": "/Portfolio/static/media/supercampShow.21d6b903.webp"
+  },
+  {
+    "revision": "80de5ceb6a72d6597791937d491b1cf1",
+    "url": "/Portfolio/static/media/supercampShow.80de5ceb.jpg"
+  },
+  {
+    "revision": "9c086f3422e97c61dc578ac88c7a8a24",
+    "url": "/Portfolio/static/media/supercampv2.9c086f34.jpg"
+  },
+  {
+    "revision": "a3242adf41defa1614909096da9809c4",
+    "url": "/Portfolio/static/media/supercampv2.a3242adf.webp"
+  },
+  {
+    "revision": "db750e7dd709ed2f609e4026071738b5",
+    "url": "/Portfolio/static/media/weatherapp.db750e7d.jpg"
+  },
+  {
+    "revision": "e8d4799ebcdde1f4299efb1d4d7b2c69",
+    "url": "/Portfolio/static/media/weatherapp.e8d4799e.webp"
+  }
+]);
